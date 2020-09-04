c58ceb8d83ec6ac6de0d6e932abcb68327d84294
Yohei Maeda <mdyh2a380824@gmail.com>
2020-09-04 16:55:40 +0900

    Add IfStmt test

--- testdata/src/IfStmt/IfStmt.go
+++ testdata/src/IfStmt/IfStmt.go
@@ -5,4 +5,15 @@ func f() {
 	if true {
 		i = i // want "variable is not initialized"
 	}
+
+	var j bool
+	if j { // want "variable is not initialized"
+		i = 1
+	}
+
+	var k bool
+	if k := true; k {
+		i = 1
+	}
+	k = k
 }
